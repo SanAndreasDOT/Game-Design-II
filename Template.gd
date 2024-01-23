d8758bc92e2cf183164c57d69efec54b4119f7fb
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2024-01-23 15:27:40 -0600

    Update Of 1/23

---
+++ Template.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_btn_show_pressed():
+	$Label.text = "Enter Text Here"
+
+
+func _on_btn_clear_pressed():
+	$Label.text = ""
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
