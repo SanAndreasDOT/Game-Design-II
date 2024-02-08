1c2ea624355828fa27c20f0288d32308767b1a8d
SanAndreasDOT <schoonover.jjanesville.k12.wi.usj@gmail.com>
2024-02-08 15:27:25 -0600

    Update of 2/8
    Changes to 3D Map

--- Player3D.gd
+++ Player3D.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 10.5
+const SPEED = 1000
+const JUMP_VELOCITY = 250
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -31,12 +31,21 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	move_and_slide()
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _ready():
+	Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	
+	
 	
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		self.rotate_y(-event.relative.x * (CAM_SENSITIVITY / 10.0))
 		camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0)) 
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
